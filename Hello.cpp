84ea696a79bbeeddc98914054fe4fae35e50d3a6
ikmalnoordin <33885122+ikmalnoordin@users.noreply.github.com>
2018-01-23 02:43:21 +0800

    string 1

---
+++ Hello.cpp
@@ -0,0 +1,16 @@
+//Nur Ikmal Hakim
+//A17DW4376
+
+#include <iostream>
+#include <cstring>
+
+int main()
+{
+	char engkau[10]="HELLO";
+
+	//stringcopy(a,"hello");
+	
+	std::cout<<engkau;
+
+	return 0;
+}
