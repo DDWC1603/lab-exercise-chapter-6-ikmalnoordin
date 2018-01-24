a94df3b6ec83bce7091f47c9573d798ad648a2e4
ikmalnoordin <33885122+ikmalnoordin@users.noreply.github.com>
2018-01-25 01:48:56 +0800

    String 3

---
+++ Password.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+int main()
+{
+	char strpswd[80];
+
+	cout<<"Enter password: ";
+	cin>>strpswd;
+	
+	if((strcmp(strpswd,"maznah46"))!=0)
+	{
+			
+		cout<<"Invalid Password "<<endl;
+	}
+	else
+	{ 
+		cout<<"Logged In "<<endl;
+	}
+	
+
+	return 0;
+}
