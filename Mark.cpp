d75056e01049c99246dd083686416cc67b129f06
ikmalnoordin <33885122+ikmalnoordin@users.noreply.github.com>
2018-03-01 18:44:47 +0800

    Array 2

---
+++ Mark.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int mark[5]={19,10,8,17,9};
+int main()
+{
+	int i;
+	//insert different value to the third element
+	//tukar third element tu dengan value lain
+	mark[2]=7;
+
+	//get user input and insert sub the third element
+	//tukar the third element yang baru ditukar tu dengan input dari user
+	cin>>mark[2];
+
+	//print the first element of the array
+	cout<<mark[0];
+
+	//print the ith element of the array
+	cin>>mark[i+1];
+
+}
